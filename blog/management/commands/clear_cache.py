0b8e2cc378de12ff27e33e56bd7663194623064c
BytedanceQA <yanweicheng@bytedance.com>
2024-06-21 10:31:47 +0000

    12

--- blog/management/commands/clear_cache.py
+++ blog/management/commands/clear_cache.py
@@ -6,6 +6,6 @@ from djangoblog.utils import cache
 class Command(BaseCommand):
     help = 'clear the whole cache'
 
-    def handle(self, *args, **options):
+    def handle(self)
         cache.clear()
         self.stdout.write(self.style.SUCCESS('Cleared cache\n'))
